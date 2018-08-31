1258899b4cfc66b39f8b0e4ddd0ef6d14e482f72
Thijs Vandaele <thijs.vandaele@live.be>
2018-08-31 08:28:28 +0200

    Add powershell script

---
+++ Sql.ps1
@@ -0,0 +1,11 @@
+<#
+    .DESCRIPTION
+        An example runbook which prints out the first 10 Azure VMs in your subscription (ordered alphabetically).
+        For more information about how this runbook authenticates to your Azure subscription, see our documentation here: http://aka.ms/fxu3mn
+
+    .NOTES
+        AUTHOR: Azure Automation Team
+        LASTEDIT: Mar 27, 2015
+#>
+
+    Write-Output "SQL"
